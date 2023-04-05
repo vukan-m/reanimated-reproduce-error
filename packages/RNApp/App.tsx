87c5754c6af2a3ccbb1079e35ee1abe22fa70845
Vukan Mirkovic <vukan@dotlab.rs>
2023-04-05 13:58:23 +0200

    update

--- packages/RNApp/App.tsx
+++ packages/RNApp/App.tsx
@@ -1,110 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import type { Node } from "react";
 import React from "react";
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
+import { SafeAreaView, Text, View } from "react-native";
+import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated, { SlideInDown } from "react-native-reanimated";
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === "dark";
+const App = () => {
+  const gesture = Gesture.Pan()
+    .onStart(() => {
+      console.log("STARTED");
+    })
+    .onUpdate(e => {
+      console.log(e);
+    });
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === "dark";
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? "light-content" : "dark-content"}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-          <Header />
+    <GestureHandlerRootView>
+      <SafeAreaView style={{ flex: 1 }}>
+        <GestureDetector gesture={gesture}>
           <View
+            collapsable={false}
             style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+              borderColor: "red",
+              borderWidth: 1,
+              height: 200,
+              width: 200,
+              backgroundColor: "yellow",
             }}
           >
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your
-              edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <Section title="Learn More">Read the docs to discover what to do next:</Section>
-            <LearnMoreLinks />
+            <Animated.View
+              entering={SlideInDown.duration(1000)}
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={{ fontSize: 20 }}>Press</Text>
+            </Animated.View>
           </View>
-        </ScrollView>
+        </GestureDetector>
       </SafeAreaView>
     </GestureHandlerRootView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-});
 
 export default App;
